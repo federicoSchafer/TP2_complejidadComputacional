e9cf1a2f8d71a8f08e23a987575e9afae85a8799
EzequielTejerina <ezequiel_tejerina@hotmail.com>
2017-09-11 21:06:51 -0300

    Se agrega funciones evaluarMSucesivas y evaluarRecursiva

---
+++ src/matematica/Polinomio.java
@@ -0,0 +1,48 @@
+package matematica;
+
+public class Polinomio {
+	private int grado;
+	private double[] coeficientes;
+	
+	public Polinomio(int n){
+		grado=n;
+		coeficientes = new double[grado+1];
+	}
+	
+	public double evaluarMSucesivas(double x ) {
+		double resultado=coeficientes[grado];
+		for(int i=0,j=grado; i<grado ;i++,j--){
+			resultado += coeficientes[i] * Math.pow(x, j);
+		}
+		return resultado;
+	}
+	
+	public double evaluarRecursiva(double x ,int n){
+		if(n == 0)
+			return coeficientes[grado];
+		return ( coeficientes[grado-n]*Math.pow(x, n) ) + evaluarRecursiva(x, n-1);
+	}
+	/*public double evaluarRecursivaPar(double x ){
+		
+	}
+	public double evaluarProgDinamica(double x ){
+		
+	}
+	public double evaluarMejorada(double x ){
+		
+	}
+	public double evaluarPow(double x ){
+		
+	}
+	public double evaluarHorner( double x ){
+		
+	}*/
+
+	public double[] getCoeficientes() {
+		return coeficientes;
+	}
+
+	public void setCoeficientes(double coeficiente,int i) {
+		this.coeficientes[i] = coeficiente;
+	}
+}
---
+++ src/test/TestPolinomio.java
@@ -0,0 +1,30 @@
+package test;
+
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+import matematica.Polinomio;
+
+public class TestPolinomio {
+
+	@Test
+	public void test() {
+		Polinomio p1 = new Polinomio(3); 
+		p1.setCoeficientes(4,0);
+		p1.setCoeficientes(3, 1);
+		p1.setCoeficientes(2, 2);
+		p1.setCoeficientes(1, 3);
+		boolean valorDeRetorno;
+		if(p1.evaluarRecursiva(1, 3) == 10)
+			valorDeRetorno=true;
+		else valorDeRetorno=false;
+		Assert.assertEquals(true, valorDeRetorno);
+		
+		
+		
+	}
+
+}
