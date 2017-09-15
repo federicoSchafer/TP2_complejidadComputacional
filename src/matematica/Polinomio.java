1fb3a8243e0e20121c450ef2cdb5f5488d388b8b
EzequielTejerina <ezequiel_tejerina@hotmail.com>
2017-09-15 00:11:49 -0300

    Se agregan metodos evaluarProgDinamina y evaluarPow

--- src/matematica/Polinomio.java
+++ src/matematica/Polinomio.java
@@ -1,5 +1,7 @@
 package matematica;
 
+import com.sun.xml.internal.bind.v2.runtime.Coordinator;
+
 public class Polinomio {
 	private int grado;
 	private double[] coeficientes;
@@ -16,33 +18,51 @@ public class Polinomio {
 	
 	public double evaluarMSucesivas(double x ) {
 		double resultado=coeficientes[grado];
-		for(int i=0,j=grado; i<grado ;i++,j--){
+		int i,j;
+		for(i=0,j=grado; i<grado ;i++,j--){
+			resultado += coeficientes[i] * myPow(x, j);
+		}
+		return resultado;
+	}
+	public double evaluarRecursiva(double x){
+		return evaluarRecursiva1(x, grado);
+	}
+	private double evaluarRecursiva1(double x ,int n){
+		if(n == 0)
+			return coeficientes[grado];
+		return ( coeficientes[grado-n] * myPow(x, n) ) + evaluarRecursiva1(x, n-1);
+	}
+	public double evaluarRecursivaPar(double x){
+		return evaluarRecursivaPar1(x,grado);
+	}
+	public double evaluarRecursivaPar1(double x ,int n){
+		if(n==0)
+			return coeficientes[grado];
+		if(n % 2 == 0)
+			return coeficientes[grado-n] * myPow(x*x, n/2) + evaluarRecursivaPar1(x, n-1);
+		return coeficientes[grado-n] * myPow(x, n) + evaluarRecursivaPar1(x, n-1);
+	}
+	public double evaluarProgDinamica(double x ){
+		double[] vectorResul = new double[grado+1];
+		int i;
+		vectorResul[grado] = coeficientes[grado] * myPow(x, 0);
+		for(i=grado-1 ; i>=0 ; i--){
+			vectorResul[i] = coeficientes[i] * myPow(x, i) + vectorResul[i+1];
+		}
+		return vectorResul[i+1];
+	}
+	public double evaluarMejorada(double x ){
+				
+		return 1;
+	}
+	public double evaluarPow(double x ){
+		double resultado=coeficientes[grado];
+		int i,j;
+		for(i=0,j=grado; i<grado ;i++,j--){
 			resultado += coeficientes[i] * Math.pow(x, j);
 		}
 		return resultado;
 	}
-	
-	public double evaluarRecursiva(double x ,int n){
-		if(n == 0)
-			return coeficientes[grado];
-		return ( coeficientes[grado-n]*Math.pow(x, n) ) + evaluarRecursiva(x, n-1);
-	}
-	public double evaluarRecursivaPar(double x ,int n){
-		if(n==0)
-			return coeficientes[grado];
-		if(n % 2 == 0)
-			return coeficientes[grado-n]*Math.pow(x*x, n/2) + evaluarRecursivaPar(x, n-1);
-		return coeficientes[grado-n]*Math.pow(x, n) + evaluarRecursivaPar(x, n-1);
-	}
-	/*public double evaluarProgDinamica(double x ){
-		
-	}
-	public double evaluarMejorada(double x ){
-		
-	}
-	public double evaluarPow(double x ){
-		
-	}*/
 	public double evaluarHorner( double x ){
 		double resultado=0;
 		for(int i=grado; i>=0; i--)
@@ -54,7 +74,14 @@ public class Polinomio {
 		return coeficientes;
 	}
 
-	public void setCoeficientes(double coeficiente,int i) {
-		this.coeficientes[i] = coeficiente;
+	public void setCoeficientes(double[] coeficiente) {
+		this.coeficientes = coeficiente;
+	}
+	
+	private double myPow(double x , int n){
+		double resultado=x;
+		for(int i=1 ; i<n ; i++)
+			resultado *= x;
+		return resultado;
 	}
 }
