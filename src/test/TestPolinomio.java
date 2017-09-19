72449673dcf193e4d1491f8a81934e2380dd500d
EzequielTejerina <ezequiel_tejerina@hotmail.com>
2017-09-18 21:17:46 -0300

    Se agregan pruebas para todos las funciones realizadas

--- src/test/TestPolinomio.java
+++ src/test/TestPolinomio.java
@@ -3,23 +3,57 @@ package test;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 import matematica.Polinomio;
 
 public class TestPolinomio {
-
-	@Test
-	public void test() {
-		Polinomio p1 = new Polinomio(3); 
-		double[] vec = {4,3,2,1};
+	private Polinomio p1;
+	private double resultado;
+	
+	@Before
+	public void setUp(){
+		p1 = new Polinomio(19); 
+		double[] vec = {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
 		p1.setCoeficientes(vec);
-		
-		Assert.assertEquals(10,p1.evaluarProgDinamica(1),0);
-		
-		
-		
+		resultado = 3.2705324314357494E18;
+	}
+	
+	@Test
+	public void calculaBienElPolinomioPorFuncionMSucesivas() {	
+		Assert.assertEquals(resultado,p1.evaluarMSucesivas(8),0);
+	}
+	
+	@Test
+	public void calculaBienElPolinomioPorFuncionRecursiva() {		
+		Assert.assertEquals(resultado,p1.evaluarRecursiva(8),0);
+	}
+	
+	@Test
+	public void calculaBienElPolinomioPorFuncionRecursivaPar() {		
+		Assert.assertEquals(resultado,p1.evaluarRecursivaPar(8),0);
 	}
 
+	@Test
+	public void calculaBienElPolinomioPorFuncionHorner() {	
+		Assert.assertEquals(resultado,p1.evaluarHorner(8),0);
+	}
+	
+	@Test
+	public void calculaBienElPolinomioPorPrograDinamica() {	
+		Assert.assertEquals(resultado,p1.evaluarProgDinamica(8),0);
+	}
+	
+	@Test
+	public void calculaBienElPolinomioPorFuncionPow() {		
+		Assert.assertEquals(resultado,p1.evaluarPow(8),0);
+	}
+	
+	@Test
+	public void calculaBienElPolinomioPorFuncionMejorada() {		
+		Assert.assertEquals(resultado,p1.evaluarMejorada(8),0);
+	}
+	
 }
